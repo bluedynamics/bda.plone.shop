a8af25926df4a3d5e72e75bf02f6979c3cb4c95d
Espen Moe-Nilssen <espen@medialog.no>
2013-06-11 12:51:26 +0200

    added default settings to registry.xml and fixed vocabulary

--- src/bda/plone/shop/vocabularies.py
+++ src/bda/plone/shop/vocabularies.py
@@ -3,19 +3,22 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.i18nmessageid import MessageFactory
 
+from bda.plone.shop.interfaces import IBdaShopSettings
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
 
 _ = MessageFactory('bda.plone.shop')
 
 
-def VatVocabulary(context):
-    items = [
-         ( '0%',  '0'), 
-         ('10%', '10'), 
-         ('20%', '20'),
-         ('25%', '25')]
-    return SimpleVocabulary.fromItems(items)
+#def OldVatVocabulary(context):
+#    items = [
+#         ( '0%',  '0'), 
+#         ('10%', '10'), 
+#         ('20%', '20'),
+#         ('25%', '25')]
+#    return SimpleVocabulary.fromItems(items)
 
-directlyProvides(VatVocabulary, IVocabularyFactory)
+#directlyProvides(VatVocabulary, IVocabularyFactory)
 
 
 def QuantityUnitVocabulary(context):
@@ -27,3 +30,22 @@ def QuantityUnitVocabulary(context):
     return SimpleVocabulary.fromItems(items)
 
 directlyProvides(QuantityUnitVocabulary, IVocabularyFactory)
+
+
+def VatVocabulary(context):
+    registry = getUtility(IRegistry)
+    settings = registry.forInterface(IBdaShopSettings)
+    if settings is None:
+        return None
+    vat_settings = settings.shop_vat
+    vat_alternatives = []
+    for line in vat_settings:
+        if line:
+            line = line.split()
+            alternative = (line[0], line[1])
+            #convert to int?
+            vat_alternatives.append(alternative)
+            
+    return SimpleVocabulary.fromItems(vat_alternatives)
+
+directlyProvides(VatVocabulary, IVocabularyFactory)
