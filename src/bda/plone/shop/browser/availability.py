99c74b20b718580acaae44b3f3c05603ec8a0b04
Robert Niederreiter <office@squarewave.at>
2013-06-18 18:43:17 +0200

    availability conditions

--- src/bda/plone/shop/browser/availability.py
+++ src/bda/plone/shop/browser/availability.py
@@ -14,3 +14,31 @@ class CartItemAvailability(CartItemAvailabilityBase):
 
     def details(self):
         return self.details_template(view=self)
+
+    @property
+    def not_available(self):
+        return not self.addable
+
+    @property
+    def full_available(self):
+        available = self.available
+        if available is None:
+            return True
+        return available > self.critical_limit
+
+    @property
+    def critical_available(self):
+        available = self.available
+        if available is None:
+            return False
+        return available > 0 and available <= self.critical_limit
+
+    @property
+    def overbook_available(self):
+        available = self.available
+        if available is None or available > 0:
+            return False
+        overbook = self.overbook
+        if overbook is None:
+            return True
+        return available > self.overbook * -1
