e501a8023f5ae1e2381b16ef3a24adfb9205edb3
Robert Niederreiter <office@squarewave.at>
2013-06-21 19:20:32 +0200

    provide cart item state

--- src/bda/plone/shop/cartdata.py
+++ src/bda/plone/shop/cartdata.py
@@ -1,13 +1,19 @@
 from decimal import Decimal
+from zope.component import getUtility
 from zope.i18n import translate
 from zope.i18nmessageid import MessageFactory
-from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
-from bda.plone.cart import CartDataProviderBase
-from bda.plone.cart.interfaces import (
-    ICartItemDataProvider,
-    ICartItemPreviewImage,
+from bda.plone.cart import (
+    readcookie,
+    extractitems,
+    aggregate_cart_item_count,
+    get_item_data_provider,
+    get_item_stock,
+    get_item_state,
+    get_item_preview,
+    CartDataProviderBase,
+    CartItemStateBase,
 )
 from .interfaces import IShopSettings
 
@@ -21,9 +27,6 @@ class CartItemCalculator(object):
     def catalog(self):
         return getToolByName(self.context, 'portal_catalog')
 
-    def data_for(self, brain):
-        return ICartItemDataProvider(brain.getObject())
-
     def net(self, items):
         cat = self.catalog
         net = Decimal(0)
@@ -31,7 +34,7 @@ class CartItemCalculator(object):
             brain = cat(UID=uid)
             if not brain:
                 continue
-            data = self.data_for(brain[0])
+            data = get_item_data_provider(brain[0].getObject())
             net += Decimal(str(data.net)) * count
         return net
 
@@ -42,10 +45,51 @@ class CartItemCalculator(object):
             brain = cat(UID=uid)
             if not brain:
                 continue
-            data = self.data_for(brain[0])
+            data = get_item_data_provider(brain[0].getObject())
             vat += (Decimal(str(data.net)) / Decimal(100)) \
                    * Decimal(str(data.vat)) * count
         return vat
+
+
+class CartItemState(CartItemStateBase):
+
+    def message(self, count):
+        items = extractitems(readcookie(self.request))
+        aggregated_count = aggregate_cart_item_count(self.context.UID(), items)
+        if not self.validate_count(aggregated_count):
+            message = _('alert_item_no_longer_available',
+                        default=u'Item is no longer available, please '
+                                u'remove from cart')
+            return translate(message, context=self.request)
+        count = float(count)
+        stock = get_item_stock(self.context)
+        available = stock.available
+        if available is None:
+            return ''
+        if available > 0 and available - count < 0:
+            reserved = available - count
+            message = _(u'alert_item_number_reserved',
+                        default=u'${reserved} items reserved',
+                        mapping={'reserved': reserved})
+            return translate(message, context=self.request)
+        elif available <= 0:
+            reserved = count
+            message =  _(u'alert_item_reserved',
+                         default=u'Reservation')
+            return translate(message, context=self.request)
+        return ''
+
+    def validate_count(self, count):
+        count = float(count)
+        stock = get_item_stock(self.context)
+        available = stock.available
+        overbook = stock.overbook
+        if available is None or overbook is None:
+            return True
+        available -= count
+        if available >= overbook * -1:
+            return True
+        return False
 
 
 class CartDataProvider(CartItemCalculator, CartDataProviderBase):
@@ -57,27 +101,30 @@ class CartDataProvider(CartItemCalculator, CartDataProviderBase):
             brain = cat(UID=uid)
             if not brain:
                 continue
-            title = brain[0].Title
-            data = self.data_for(brain[0])
+            brain = brain[0]
+            obj = brain.getObject()
+            title = brain.Title
+            data = get_item_data_provider(obj)
             price = Decimal(str(data.net)) * count
             if data.display_gross:
                 price = price + price / Decimal(100) * Decimal(str(data.vat))
-            url = brain[0].getURL()
-            description = brain[0].Description
+            url = brain.getURL()
+            description = brain.Description
             comment_required = data.comment_required
             quantity_unit_float = data.quantity_unit_float
             quantity_unit = translate(data.quantity_unit, context=self.request)
-            preview_image_url = ICartItemPreviewImage(brain[0].getObject()).url
-            aggregated_count = self.aggregate_count_for(uid, items)
-            valid_count = self.validate_count(uid, aggregated_count)['success']
-            no_longer_available = not valid_count
-            ret.append(self.item(uid, title, count, price, url, comment,
-                                 description, comment_required,
-                                 quantity_unit_float, quantity_unit,
-                                 preview_image_url, no_longer_available))
+            preview_image_url = get_item_preview(obj).url
+            item_state = get_item_state(obj, self.request)
+            no_longer_available = not item_state.validate_count(count)
+            alert = item_state.message(count)
+            item = self.item(
+                uid, title, count, price, url, comment, description,
+                comment_required, quantity_unit_float, quantity_unit,
+                preview_image_url, no_longer_available, alert)
+            ret.append(item)
         return ret
 
-    @property 
+    @property
     def _settings(self):
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IShopSettings)
