ad3767b7ed27f0898792c006ea0c465f7abe45d3
Johannes Raggam <raggam-nl@adm.at>
2013-12-07 01:29:50 +0100

    add delivery address

--- src/bda/plone/shop/userdata.py
+++ src/bda/plone/shop/userdata.py
@@ -10,7 +10,21 @@ from zope.interface import Interface
 from bda.plone.shop import message_factory as _
 
 
+def validate_accept(value):
+    if value is not True:
+        return False
+    return True
+
+
 class IAddress(model.Schema):
+    model.fieldset('main_address', _('main_address', u'Hauptadresse'),
+                   fields=['street', 'zip_code', 'city', 'country', 'phone'])
+    model.fieldset('delivery_address',
+                   _('delivery_address', u'Zustelladresse'),
+                   fields=['delivery_street', 'delivery_zip_code',
+                           'delivery_city', 'delivery_country',
+                           'delivery_phone'])
+
     street = schema.TextLine(
         title=_(u'label_street', default=u'Street'),
         description=_(u'help_street', default=u''),
@@ -25,19 +39,72 @@ class IAddress(model.Schema):
         title=_(u'label_city', default=u'City'),
         description=_(u'help_city', default=u""),
         required=False,
-        )
+    )
     country = schema.Choice(
         title=_(u'label_country', default=u'Country'),
         description=_(u'help_country', default=u""),
         required=False,
         vocabulary='collective.address.CountryVocabulary'
-        )
+    )
     phone = schema.TextLine(
         title=_(u'label_phone', default=u'Telephone number'),
         description=_(u'help_phone',
                       default=u"Leave your phone number so we can reach you."),
         required=False,
-        )
+    )
+
+    delivery_address = schema.Bool(
+        title=_(u'label_delivery_address',
+                default=u'Delivery address different'),
+        description=_(u'help_accept',
+                      default=u"Delivery address is different from payment."),
+        required=True,
+        constraint=validate_accept,
+    )
+
+    # Delivery Address
+    delivery_name = schema.TextLine(
+        title=_(u'label_name', default=u'Name'),
+        description=_(u'help_name', default=u''),
+        required=False
+    )
+    delivery_street = schema.TextLine(
+        title=_(u'label_street', default=u'Street'),
+        description=_(u'help_street', default=u''),
+        required=False
+    )
+    delivery_zip_code = schema.TextLine(
+        title=_(u'label_zip_code', default=u'Zip Code'),
+        description=_(u'help_zip_code', default=u''),
+        required=False
+    )
+    delivery_city = schema.TextLine(
+        title=_(u'label_city', default=u'City'),
+        description=_(u'help_city', default=u""),
+        required=False,
+    )
+    delivery_country = schema.Choice(
+        title=_(u'label_country', default=u'Country'),
+        description=_(u'help_country', default=u""),
+        required=False,
+        vocabulary='collective.address.CountryVocabulary'
+    )
+    delivery_phone = schema.TextLine(
+        title=_(u'label_phone', default=u'Telephone number'),
+        description=_(u'help_phone',
+                      default=u"Leave your phone number so we can reach you."),
+        required=False,
+    )
+
+    accept = schema.Bool(
+        title=_(u'label_accept', default=u'Accept terms of use'),
+        description=_(u'help_accept',
+                      default=u"Tick this box to indicate that you have found,"
+                      " read and accepted the terms of use for this site. "),
+        required=True,
+        constraint=validate_accept,
+    )
+
 
 
 from plone.app.users.browser.account import AccountPanelSchemaAdapter
@@ -50,6 +117,7 @@ class UserDataPanelExtender(extensible.FormExtender):
 
     def update(self):
         fields = field.Fields(IAddress)
+        fields = fields.omit('accept')  # Users have already accepted.
         self.add(fields, prefix="delivery")
 
 
@@ -66,4 +134,6 @@ class AddUserFormExtender(extensible.FormExtender):
 
     def update(self):
         fields = field.Fields(IAddress)
+        # management form doesn't need this field
+        fields = fields.omit('accept')
         self.add(fields, prefix="delivery")
