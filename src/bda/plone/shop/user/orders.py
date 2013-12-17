855f7a909a14ebaba27f706f6725353c5a258002
Johannes Raggam <raggam-nl@adm.at>
2013-12-17 01:20:54 +0100

    userorderstable for custom renderings

--- src/bda/plone/shop/user/orders.py
+++ src/bda/plone/shop/user/orders.py
@@ -1,10 +1,68 @@
-from bda.plone.orders.browser.views import OrdersData
+from bda.plone.orders import message_factory as _bpo
+from bda.plone.orders.browser.views import TableData
+from bda.plone.orders.browser.views import OrdersTable
+from bda.plone.orders.browser.views import Translate
+from plone.api import user as apiuser
 from repoze.catalog.query import Contains
 from repoze.catalog.query import Eq
-from plone.api import user as apiuser
+from yafowil.utils import Tag
 
 
-class UserOrdersData(OrdersData):
+class UserOrdersTable(OrdersTable):
+
+    @property
+    def columns(self):
+        return [{
+            'id': 'actions',
+            'label': _bpo('actions', 'Actions'),
+            'renderer': self.render_order_actions,
+        }, {
+            'id': 'created',
+            'label': _bpo('date', 'Date'),
+            'renderer': self.render_dt,
+        }, {
+            'id': 'personal_data.lastname',
+            'label': _bpo('lastname', 'Last Name'),
+        }, {
+            'id': 'personal_data.firstname',
+            'label': _bpo('firstname', 'First Name'),
+        }, {
+            'id': 'billing_address.city',
+            'label': _bpo('city', 'City'),
+        }, {
+            'id': 'salaried',
+            'label': _bpo('salaried', 'Salaried'),
+            'renderer': self.render_salaried,
+        }, {
+            'id': 'state',
+            'label': _bpo('state', 'State'),
+            'renderer': self.render_state,
+        }]
+
+    def render_order_actions(self, colname, record):
+        tag = Tag(Translate(self.request))
+        target = '%s?uid=%s' % (self.context.absolute_url(),
+                                str(record.attrs['uid']))
+        link_attrs = {
+            'ajax:bind': 'click',
+            'ajax:target': target,
+            'ajax:overlay': 'order',
+            'class_': 'contenttype-document',
+            'href': '',
+            'title': _bpo('view_order', 'View Order'),
+        }
+        return tag('a', '&nbsp', **link_attrs)
+
+    def render_salaried(self, colname, record):
+        return record.attrs.get('salaried', 'no')
+
+    def render_state(self, colname, record):
+        return record.attrs['state']
+
+
+class UserOrdersData(UserOrdersTable, TableData):
+    soup_name = 'bda_plone_orders_orders'
+    search_text_index = 'text'
 
     def query(self, soup):
         user = apiuser.get_current()
