1cf1c9c3491b9a77394285036f5430f106ea4e68
Jens W. Klein <jens@bluedynamics.com>
2014-01-30 15:54:34 +0100

    pep8

--- src/bda/plone/shop/interfaces.py
+++ src/bda/plone/shop/interfaces.py
@@ -1,6 +1,6 @@
 from zope.interface import Interface
 from zope import schema
-from zope.interface import alsoProvides
+from zope.interface import provider
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from plone.supermodel import model
 from plone.autoform.directives import widget
@@ -40,29 +40,37 @@ class IShopSettings(model.Schema):
 
     admin_email = schema.ASCIILine(
         title=_(u"label_admin_email", default=u'Shop Admin E-Mail'),
-        description=_(u"help_admin_email",
-                      default=u'No typos please....'),
+        description=_(u"help_admin_email", default=u'No typos please....'),
         required=True,
-        default="")
+        default=""
+    )
 
     default_item_display_gross = schema.Bool(
-        title=_(u'label_default_item_display_gross',
-                default=u'Display Gross by default'),
-        required=False)
+        title=_(
+            u'label_default_item_display_gross',
+            default=u'Display Gross by default'
+        ),
+        required=False
+    )
 
     currency = schema.Choice(
         title=_(u"label_currency", default="Currency"),
-        description=_(u"help_currency",
-                      default=u"Choose the default currency"),
-        vocabulary='bda.plone.shop.vocabularies.AvailableCurrenciesVocabulary')
+        description=_(
+            u"help_currency",
+            default=u"Choose the default currency"
+        ),
+        vocabulary='bda.plone.shop.vocabularies.AvailableCurrenciesVocabulary'
+    )
 
     show_currency = schema.Choice(
         title=_(u"label_show_currency", default=u"Show the currency for items"),
         description=_(u"help_show_currency", default=u""),
         vocabulary=
-            'bda.plone.shop.vocabularies.CurrencyDisplayOptionsVocabulary')
+            'bda.plone.shop.vocabularies.CurrencyDisplayOptionsVocabulary'
+    )
 
 
+@provider(IShopSettingsProvider)
 class IShopCartSettings(model.Schema):
     """Shop controlpanel schema for cart settings.
     """
@@ -75,38 +83,50 @@ class IShopCartSettings(model.Schema):
             'summary_total_only',
             'show_checkout',
             'show_to_cart',
-            ],
-        )
+        ],
+    )
 
     disable_max_article = schema.Bool(
         title=_(u"label_disable_max_article", default=u"Disable max article"),
-        description=_(u"help_disable_max_article",
-                      default=u"No total number of items in cart limit"),
-        default=True)
+        description=_(
+            u"help_disable_max_article",
+            default=u"No total number of items in cart limit"
+        ),
+        default=True
+    )
 
     summary_total_only = schema.Bool(
-        title=_(u"label_summary_total_only",
-                default=u"Cart Summary total only"),
-        description=_(u"help_summary_total_only",
-                      default=u"Show only total value in cart summary"),
-        default=False)
+        title=_(
+            u"label_summary_total_only",
+            default=u"Cart Summary total only"
+        ),
+        description=_(
+            u"help_summary_total_only",
+            default=u"Show only total value in cart summary"
+        ),
+        default=False
+    )
 
     show_checkout = schema.Bool(
-        title=_(u"label_show_checkout",
-                default=u"Show checkout link in portlet"),
+        title=_(
+            u"label_show_checkout",
+            default=u"Show checkout link in portlet"
+        ),
         description=_(u"help_show_checkout", default=""),
-        default=False)
+        default=False
+    )
 
     show_to_cart = schema.Bool(
-        title=_(u"label_show_to_cart",
-                default=u"Show link to cart in portlet"),
+        title=_(
+            u"label_show_to_cart",
+            default=u"Show link to cart in portlet"
+        ),
         description=_(u"help_show_to_cart", default=u""),
-        default=True)
+        default=True
+    )
 
 
-alsoProvides(IShopCartSettings, IShopSettingsProvider)
-
-
+@provider(IShopSettingsProvider)
 class IShopArticleSettings(model.Schema):
     """Shop controlpanel schema for article settings.
     """
@@ -121,52 +141,73 @@ class IShopArticleSettings(model.Schema):
             'default_item_comment_enabled',
             'default_item_comment_required',
             'default_item_quantity_unit_float',
-            ],
-        )
+        ],
+    )
 
     widget('quantity_units', CheckBoxFieldWidget)
     quantity_units = schema.List(
-        title=_(u"label_quantity_units",
-                default=u"Specify quantity units allowed in shop."),
-        description=_(u"help_quantity_units",
-                      default=u'Quantity units (what the buyable items are '
-                              u'measured in)'),
+        title=_(
+            u"label_quantity_units",
+            default=u"Specify quantity units allowed in shop."
+        ),
+        description=_(
+            u"help_quantity_units",
+            default=u'Quantity units (what the buyable items are measured in)'
+        ),
         required=True,
         missing_value=set(),
         value_type=schema.Choice(
             vocabulary=
-                'bda.plone.shop.vocabularies.AvailableQuantityUnitVocabulary'))
+                'bda.plone.shop.vocabularies.AvailableQuantityUnitVocabulary'
+        )
+    )
 
     default_item_quantity_unit = schema.Choice(
-        title=_(u"label_default_quantity_units",
-                default=u"Specify default quantity name."),
-        description=_(u"help_default_quantity_unit",
-                      default=u'default measurement'),
-        vocabulary='bda.plone.shop.vocabularies.QuantityUnitVocabulary')
+        title=_(
+            u"label_default_quantity_units",
+            default=u"Specify default quantity name."
+        ),
+        description=_(
+            u"help_default_quantity_unit",
+            default=u'default measurement'
+        ),
+        vocabulary='bda.plone.shop.vocabularies.QuantityUnitVocabulary'
+    )
 
     default_item_net = schema.Float(
-        title=_(u'label_default_item_net', default=u'Default Item net price'),
-        required=False)
+        title=_(
+            u'label_default_item_net',
+            default=u'Default Item net price'
+        ),
+        required=False
+    )
 
     default_item_comment_enabled = schema.Bool(
-        title=_(u'label_default_item_comment_enabled',
-                default='Comment enabled by default'),
-        required=False)
+        title=_(
+            u'label_default_item_comment_enabled',
+            default='Comment enabled by default'
+        ),
+        required=False
+    )
 
     default_item_comment_required = schema.Bool(
-        title=_(u'label_default_item_comment_required',
-                default='Comment required by default'),
-        required=False)
+        title=_(
+            u'label_default_item_comment_required',
+            default='Comment required by default'
+        ),
+        required=False
+    )
 
     default_item_quantity_unit_float = schema.Bool(
-        title=_(u'label_default_item_quantity_unit_float',
-                default='Quantity as float as default'),
-        required=False)
+        title=_(
+            u'label_default_item_quantity_unit_float',
+            default='Quantity as float as default'
+        ),
+        required=False
+    )
 
 
-alsoProvides(IShopArticleSettings, IShopSettingsProvider)
-
-
+@provider(IShopSettingsProvider)
 class IShopShippingSettings(model.Schema):
     """Shop controlpanel schema for article settings.
     """
@@ -177,26 +218,27 @@ class IShopShippingSettings(model.Schema):
         fields=[
             'include_shipping_costs',
             'shipping_method',
-            ],
-        )
+        ],
+    )
 
     include_shipping_costs = schema.Bool(
-        title=_(u"label_include_shipping_costs",
-                default=u"Include Shipping Costs"),
-        description=_(u"help_include_shipping_costs",
-                      default=u""),
-        default=True)
+        title=_(
+            u"label_include_shipping_costs",
+            default=u"Include Shipping Costs"
+        ),
+        description=_(u"help_include_shipping_costs", default=u""),
+        default=True
+    )
 
     shipping_method = schema.Choice(
         title=_(u"label_shipping_method", default=u"Shipping Method"),
         description=_(u"help_shipping_method", default=u""),
         vocabulary=
-            'bda.plone.shop.vocabularies.AvailableShippingMethodsVocabulary')
+            'bda.plone.shop.vocabularies.AvailableShippingMethodsVocabulary'
+    )
 
 
-alsoProvides(IShopShippingSettings, IShopSettingsProvider)
-
-
+@provider(IShopSettingsProvider)
 class IShopTaxSettings(model.Schema):
     """Shop controlpanel schema for tax settings.
     """
@@ -207,25 +249,57 @@ class IShopTaxSettings(model.Schema):
         fields=[
             'vat',
             'default_item_vat',
-            ],
-        )
+        ],
+    )
 
     widget('vat', CheckBoxFieldWidget)
     vat = schema.List(
         title=_(u"label_vat", default=u'VAT in %'),
-        description=_(u"help_vat",
-                      default=u"Specify all allowed vat settings"),
+        description=_(
+            u"help_vat",
+            default=u"Specify all allowed vat settings"
+        ),
         required=True,
         missing_value=set(),
         value_type=schema.Choice(
-            vocabulary=\
-                'bda.plone.shop.vocabularies.AvailableVatVocabulary'))
+            vocabulary='bda.plone.shop.vocabularies.AvailableVatVocabulary'
+        )
+    )
 
     default_item_vat = schema.Choice(
         title=_(u"label_default_vat", default=u'Default Value added tax name'),
-        description=_(u"help_default_vat",
-                      default=u"Specify default vat name"),
-        vocabulary='bda.plone.shop.vocabularies.VatVocabulary')
+        description=_(
+            u"help_default_vat",
+            default=u"Specify default vat name"
+        ),
+        vocabulary='bda.plone.shop.vocabularies.VatVocabulary'
+    )
 
 
-alsoProvides(IShopTaxSettings, IShopSettingsProvider)
+@provider(IShopSettingsProvider)
+class INotificationTextSettings(model.Schema, INotificationText):
+
+    model.fieldset(
+        'notifications',
+        label=_(u'Notifications'),
+        fields=[
+            'order_text',
+            'overbook_text',
+        ],
+    )
+
+    order_text = schema.Text(
+        title=_(
+            u"label_order_notification_text",
+            default=u"Order Notification Text"
+        ),
+        required=False
+    )
+
+    overbook_text = schema.Text(
+        title=_(
+            u"label_overbook_notification_text",
+            default=u"Overbook Notification Text"
+        ),
+        required=False
+    )
