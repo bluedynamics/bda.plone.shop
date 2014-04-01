485643a94f0c929185275378551586073e8a6c7e
Johannes Raggam <raggam-nl@adm.at>
2014-04-02 01:27:58 +0200

    meke viewlet class obsolete

--- src/bda/plone/shop/browser/viewlets.py
+++
@@ -1,9 +0,0 @@
-from plone.app.layout.viewlets.common import ViewletBase
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
-class BuyableViewlet(ViewletBase):
-    """Viewlet rendering buyable controls.
-    """
-
-    index = ViewPageTemplateFile('buyable_viewlet.pt')
