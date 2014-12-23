29479cf9be6017eab4a83ec3265e5929e5966f30
Peter Holzer <hpeter@agitator.com>
2014-12-23 19:49:25 +0100

    "bda.recipe.deployment pre tag commit"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '0.10.dev0'
+version = '0.10.dev0.sfama'
 shortdesc = "Shop Solution for Plone"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
