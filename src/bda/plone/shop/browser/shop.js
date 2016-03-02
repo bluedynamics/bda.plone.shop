69d267029925c6cc0e9d528a0274494cfb51df6c
Peter Holzer <hpeter@agitator.com>
2016-03-02 17:04:14 +0100

    js require wrapper

--- src/bda/plone/shop/browser/shop.js
+++ src/bda/plone/shop/browser/shop.js
@@ -1,31 +1,40 @@
-/* jslint browser: true */
-/* global jQuery, bdajax */
-(function($, bdajax) {
-    "use strict";
+/*jslint browser: true*/
+/*global $, jQuery, plone, require*/
 
-    $(document).ready(function() {
-        var binder = function(context) {
-            $('div.availability', context).unbind('mouseover')
-                                          .bind('mouseover', function() {
-                var details = $('div.availability_details', $(this));
-                if (!details.is(":visible")) {
-                    details.show();
-                }
-            });
-            $('div.availability', context).unbind('mouseout')
-                                          .bind('mouseout', function() {
-                var details = $('div.availability_details', $(this));
-                if (details.is(":visible")) {
-                    details.hide();
-                }
-            });
-        };
-        if (bdajax !== undefined) {
-            $.extend(bdajax.binders, {
-                buyable_controls_binder: binder
-            });
-        }
-        binder(document);
-    });
+if(require === undefined){
+  require = function(reqs, torun){
+    'use strict';
+    return torun(window.jQuery);
+  }
+}
 
-})(jQuery, bdajax);
+require([
+    'jquery',
+    'bdajax'
+], function($){
+    'use strict';
+
+    var binder = function(context) {
+        $('div.availability', context).unbind('mouseover')
+                                      .bind('mouseover', function() {
+            var details = $('div.availability_details', $(this));
+            if (!details.is(":visible")) {
+                details.show();
+            }
+        });
+        $('div.availability', context).unbind('mouseout')
+                                      .bind('mouseout', function() {
+            var details = $('div.availability_details', $(this));
+            if (details.is(":visible")) {
+                details.hide();
+            }
+        });
+    };
+    if (bdajax !== undefined) {
+        $.extend(bdajax.binders, {
+            buyable_controls_binder: binder
+        });
+    }
+    binder(document);
+
+});
