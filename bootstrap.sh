1d53f08db8c26c5dbe43b0e3abab5ee30c3e1e22
Jens W. Klein <jk@kleinundpartner.at>
2016-09-16 12:14:01 +0200

    use specific

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-rm -r ./lib ./include ./local ./bin
-virtualenv --clear .
-./bin/pip install -r requirements.txt
-./bin/buildout
