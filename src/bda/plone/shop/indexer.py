af0b4179cdc5cc45e6b403898702f0e559cc4950
Johannes Raggam <thetetet@gmail.com>
2017-06-20 01:07:19 +0200

    Add metadata columns for ``item_net``, ``item_buyable_start`` and ``item_buyable_end``. This way, they can be displayed in the table view of collection results.

--- src/bda/plone/shop/indexer.py
+++ src/bda/plone/shop/indexer.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
+from bda.plone.cart import get_item_data_provider
 from bda.plone.orders.interfaces import IBuyable
+from bda.plone.shop.interfaces import IBuyablePeriod
+from decimal import Decimal
 from plone.indexer import indexer
+from zope.component import queryAdapter
 
 
 @indexer(IBuyable)
 def item_buyable(obj):
     return True
+
+
+@indexer(IBuyable)
+def item_net(obj):
+    item_data = get_item_data_provider(obj)
+    return Decimal(item_data.net)
+
+
+@indexer(IBuyable)
+def item_buyable_start(obj):
+    buyable_period = queryAdapter(obj, IBuyablePeriod)
+    if not buyable_period or not buyable_period.effective:
+        raise AttributeError
+    return buyable_period.effective
+
+
+@indexer(IBuyable)
+def item_buyable_end(obj):
+    buyable_period = queryAdapter(obj, IBuyablePeriod)
+    if not buyable_period or not buyable_period.expires:
+        raise AttributeError
+    return buyable_period.expires
