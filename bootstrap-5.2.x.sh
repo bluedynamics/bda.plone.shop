8d5c38e2dbb81ebf141fe6c5bf9020e084cd2e14
Jens W. Klein <jk@kleinundpartner.at>
2019-05-15 17:11:43 +0200

    ignore pyenv files and fix buildout

--- bootstrap-5.2.x.sh
+++ bootstrap-5.2.x.sh
@@ -2,6 +2,6 @@
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
 rm -r ./lib ./include ./local ./bin
-ln -fs plone-5.1.x.cfg buildout.cfg
+ln -fs plone-5.2.x.cfg buildout.cfg
 virtualenv --clear .
-./bin/pip install r http://dist.plone.org/release/5.1-latest/requirements.txt
+./bin/pip install r http://dist.plone.org/release/5.2-latest/requirements.txt
