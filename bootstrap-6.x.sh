373481febbfee4b23e85720655ac9a8bcc401e90
Robert Niederreiter <office@squarewave.at>
2021-02-19 07:49:13 +0100

    Restore venv support, fix versions

--- bootstrap-6.x.sh
+++ bootstrap-6.x.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
-rm -r ./lib ./include ./local ./bin
-ln -fs plone-6.x.cfg buildout.cfg
-pyenv virtualenv 3.8.6 bda.plone.shop
-pyenv local bda.plone.shop
-pip install -r https://raw.githubusercontent.com/plone/buildout.coredev/6.0/requirements.txt
-buildout
+#!/bin/bash
+set -e
+
+REQUIREMENTS="https://raw.githubusercontent.com/plone/buildout.coredev/6.0/requirements.txt"
+
+for dir in lib include local bin share pyvenv.cfg lib64; do
+    if [ -d "$dir" ]; then
+        echo "Removing $dir..."
+        rm -r "$dir"
+    fi
+done
+
+if [ "$1" == "venv" ]; then
+    ln -fs plone-6.x.cfg buildout.cfg
+    python3 -m venv .
+    ./bin/pip install -r $REQUIREMENTS
+    ./bin/buildout
+else
+
+    pyenv virtualenv 3.8.6 bda.plone.shop
+    pyenv local bda.plone.shop
+    pip install -r $REQUIREMENTS
+    buildout
+fi
